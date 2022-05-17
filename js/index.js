91d8b0b12a9f13bfb63cc8620c9938419337a6ea
程骞 <100251881+User-ChengQian@users.noreply.github.com>
2022-05-17 23:46:32 +0800

    footer

--- js/index.js
+++ js/index.js
@@ -1,13 +1,36 @@
 $(function(){
-     for(i=1;i<=6;i++){
-      $(".bg_btn li a").mouseover(function(){
-       var index=$(this).attr("index")-1
-        $(".tags li:eq("+index+")").css("display","block").addClass("downpx");
+      // $(".bg_btn li ").hover(
+      //   function(){
+      //   var index=$(this).children().attr("index")-1
+      //  console.log(11111111111);
+      //   $(".tags li:eq("+index+")").stop().css("display","block").stop().animate({
+      //    top:"0px"
+      //   },300);
+      //   },
+      //   function(){
+      //  console.log(222222222222);
+      //     var index=$(this).children().attr("index")-1
+      //    $(".tags li:eq("+index+")").stop().css("display","none").stop().animate({
+      //     top:"0px"
+      //    },300);
+      //   },
+      // )
+      // --------------------------------
+      $(".bg_btn li").mouseenter(function(){
+       var index=$(this).children().attr("index")-1
+       console.log(1);
+        $(".tags li:eq("+index+")").stop().css("display","block").stop().animate({
+         top:"10px"
+        },300);
+        
       })
-      $(".bg_btn li a").mouseleave(function(){
-        var index=$(this).attr("index")-1
-         $(".tags li:eq("+index+")").css("display","none").removeClass("downpx");
-       })
 
-     }                    
+      $(".bg_btn li").mouseleave(function(){
+        var index=$(this).children().attr("index")-1
+         $(".tags li:eq("+index+")").stop().css("display","none").stop().animate({
+          top:"0px"
+         },300);
+       })   
+       
+      
 })
