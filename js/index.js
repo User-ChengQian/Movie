0e1709a516801e0f9f67bed0afa1029e814d2111
程骞 <100251881+User-ChengQian@users.noreply.github.com>
2022-05-25 23:40:36 +0800

    无法js写入接口

--- js/index.js
+++ js/index.js
@@ -13,21 +13,47 @@ $(function(){
          },300);
         },
       ) 
-      //  $(".user a").click(function(e){
-      //    e.preventDefault();
-      //    console.log(11);
-      //    window.location.href="./user.html"
-      //  })
-       $(".content>div").click(function(){
-         window.location.href="./movie.html"
-       })
-       $(".bg_btn li>a").click(function(){
-         $(".tips").stop().animate({ 
-       opacity: 1,
-        top:"-100px"
-        }, 1500 ,function(){
-          $(".tips").css({top:"-30px",opacity:0})
-        });
-       })
       
+      $(".bg_btn li>a").click(function(){
+        animateCSS('.tips', 'bounceIn');
+      })
+
+      // div点击事件
+      for(i=0;i<11;i++){
+        $(`.content .box>div:eq(${i})`).click(function(){
+          var index =$(this).attr("data-index")
+          console.log(index);
+          //存储当前点击的自定义属性(=id)
+          localStorage.setItem("id",`${index}`)
+          //跳转
+          location.href="./movie.html"
+        })
+      }
+
+  // 排版样式1
+ arr1= arr.filter(function(item){
+    return item.size==1
+  })
+  arr1.forEach(function(item,i){
+    // 设置自定义属性
+  $(`.content .s_box:eq(${i})`).attr("data-index",`${item.id}`)
+  //内容渲染
+    $(`.content .s_box:eq(${i}) img`).attr("src",`./src/mysql/pic/${item.id}.png`)
+    $(`.content .s_box:eq(${i}) p i`).text(item.value)
+    $(`.content .s_box:eq(${i}) p:eq(1)`).text(item.name)
+  })
+  // 排版样式2
+  arr2= arr.filter(function(item){
+    return item.size==2
+  })
+  arr2.forEach(function(item,i){
+  $(`.content .okay:eq(${i})`).attr("data-index",`${item.id}`)
+    $(`.content .okay:eq(${i}) img`).attr("src",`./src/mysql/pic/${item.id}.png`)
+    $(`.content .okay:eq(${i}) p:eq(0)`).text(item.name)
+    $(`.content .okay:eq(${i}) p:eq(1) i`).text(item.loves)
+    $(`.content .okay:eq(${i}) p:eq(2) i`).text(item.com)
+    $(`.content .okay:eq(${i}) p:eq(3) i`).text(item.share)
+    $(`.content .okay:eq(${i}) p:eq(4) i`).text(item.update)
+
+  })
 })
--- js/movie.js
+++ js/movie.js
@@ -1,4 +1,13 @@
+  
 $(function(){
+  var id =localStorage.getItem("id")
+  arr.forEach(item => {
+  if(item.id==id){
+  $('button').html(item.name)
+  $('.main .watch #ckplays #jiekou').porp('src',`https://svip.bljiex.cc/?v= `+`${item.url}`)
+    
+  } 
+});   
   $(".back button").click(function(){
     window.location.href="./index.html"
   })
